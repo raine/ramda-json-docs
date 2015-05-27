ba20ac78cb531c9a2e60e436374fbb3a4f9bb93a
Raine Virta <raine.virta@gmail.com>
2015-05-28 00:57:23 +0300

    move stuff to etc

---
+++ etc/update-readme.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cat latest.json | R \
+  'project <[ name sig category ]>' \
+  'map evolve sig: (-> it && "`#it`"), name: -> "[`#it`](http://ramdajs.com/docs/#it)"' \
+  | md-table \
+  | R -i raw -s 'join \\n' 'create-map-entry \table' \
+  | tmpl README.tmpl.md > README.md
