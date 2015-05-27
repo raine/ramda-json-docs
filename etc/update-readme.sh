6c046c193760cd7e20d13a3cd27a659b1c18e1ce
Raine Virta <raine.virta@gmail.com>
2015-05-28 01:01:41 +0300

    rename R to ramda

--- etc/update-readme.sh
+++ etc/update-readme.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cat latest.json | R \
+cat latest.json | ramda \
   'project <[ name sig category ]>' \
   'map evolve sig: (-> it && "`#it`"), name: -> "[`#it`](http://ramdajs.com/docs/##it)"' \
   | md-table \
-  | R -i raw -s 'join \\n' 'create-map-entry \table' \
+  | ramda -i raw -s 'join \\n' 'create-map-entry \table' \
   | tmpl etc/README.tmpl.md > README.md
