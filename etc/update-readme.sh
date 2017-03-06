eb44bd1b14fdcbed61c26aa87e58908c783351e6
Raine Virta <raine.virta@gmail.com>
2017-03-06 19:48:21 +0200

    update-readme: create-map-entry -> obj-of

--- etc/update-readme.sh
+++ etc/update-readme.sh
@@ -4,5 +4,5 @@ cat $DOCS/latest.json | ramda \
   'project <[ name sig category ]>' \
   'map evolve sig: (-> it && "`#it`"), name: -> "[`#it`](http://ramdajs.com/docs/##it)"' \
   | md-table \
-  | ramda -i raw -s 'join \\n' 'create-map-entry \table' \
+  | ramda -i raw -s 'join \\n' 'obj-of \table' \
   | tmpl $DOCS/etc/README.tmpl.md > $DOCS/README.md
