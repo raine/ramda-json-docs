fb26e9d701ad75aa1b8d9a5f2c8610e161e3a28f
Raine Virta <raine.virta@gmail.com>
2017-03-06 19:47:55 +0200

    update-readme: use absolute paths with $DOCS

--- etc/update-readme.sh
+++ etc/update-readme.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cat ../latest.json | ramda \
+cat $DOCS/latest.json | ramda \
   'project <[ name sig category ]>' \
   'map evolve sig: (-> it && "`#it`"), name: -> "[`#it`](http://ramdajs.com/docs/##it)"' \
   | md-table \
   | ramda -i raw -s 'join \\n' 'create-map-entry \table' \
-  | tmpl README.tmpl.md > ../README.md
+  | tmpl $DOCS/etc/README.tmpl.md > $DOCS/README.md
