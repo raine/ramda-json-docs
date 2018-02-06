2d1a10f43bafa4212416f6ae5ec6038fc57f8024
Raine Virta <raine.virta@gmail.com>
2018-02-06 14:33:37 +0200

    update-readme: escape pipes in table

--- etc/update-readme.sh
+++ etc/update-readme.sh
@@ -2,7 +2,7 @@
 
 cat $DOCS/latest.json | ramda \
   'project <[ name sig category ]>' \
-  'map evolve sig: (-> it && "`#it`"), name: -> "[`#it`](http://ramdajs.com/docs/##it)"' \
+  'map evolve sig: (-> it && "`#{it.replace(/\|/g, "\\|")}`"), name: -> "[`#it`](http://ramdajs.com/docs/##it)"' \
   | md-table \
   | ramda -i raw -s 'join \\n' 'obj-of \table' \
   | tmpl $DOCS/etc/README.tmpl.md > $DOCS/README.md
